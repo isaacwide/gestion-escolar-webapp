98b6090c7e080f7c5b0c485f4a72404e75fda4f3
isaacwide <isaac.aguilard@alumno.buap.mx>
2026-03-12 11:13:19 -0600

    implementacion login

--- src/app/screens/login-screen/login-screen.ts
+++ src/app/screens/login-screen/login-screen.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
-import {MatBadgeModule} from '@angular/material/badge';
+import { Component, OnInit } from '@angular/core';
+import { SHARED_IMPORTS } from '../../shared/shared.imports';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [MatBadgeModule],
+  imports: [
+    ...SHARED_IMPORTS
+  ],
   templateUrl: './login-screen.html',
   styleUrl: './login-screen.scss',
 })
-export class LoginScreen {
-  hidden: boolean = false; // ← agrega esta línea
-  toggleBadgeVisibility() {
-    this.hidden = !this.hidden;
+export class LoginScreen implements OnInit{
+  // aqui va las variables globales 
+  public username:string='';
+  public password:string='';
+  public load:boolean=false;
+  public error:any={};
+  public type:string="password"
+
+  constructor( 
+    public router:Router
+  ){}
+  // primera ves que  queremos que se ejecute la aplicacion 
+  ngOnInit(): void {
+    
   }
+
+  public login(){
+
+  }
+  public registrar(){
+
+  }
+
+  public showPassword(){
+
+  }
+
+
 }
---
+++ src/app/shared/shared.imports.ts
@@ -0,0 +1,15 @@
+// aca va estar la coleccion me modulos 
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common';
+import {RouterModule} from '@angular/router'; 
+
+export const SHARED_IMPORTS =[
+    
+  CommonModule,
+  FormsModule,
+  ReactiveFormsModule,
+  RouterModule,
+  NgOptimizedImage,
+
+];

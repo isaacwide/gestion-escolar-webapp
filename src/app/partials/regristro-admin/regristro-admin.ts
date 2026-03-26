f5bb1ee7cfc4de420d6964e599df6f7c38dac841
isaacwide <isaac.aguilard@alumno.buap.mx>
2026-03-26 13:41:27 -0600

    mas intrigas

--- src/app/partials/regristro-admin/regristro-admin.ts
+++ src/app/partials/regristro-admin/regristro-admin.ts
@@ -1,14 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { SHARED_IMPORTS } from '../../shared/shared.imports';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-registro-admin',
-  imports: [],
+  imports: [MatFormField, MatLabel ,MatDatepickerModule
+    ,...SHARED_IMPORTS
+  ],
   templateUrl: './regristro-admin.html',
   styleUrl: './regristro-admin.scss',
 })
-export class RegristroAdmin {
+export class RegristroAdmin implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public admin:any={};
+  public errors:any={};
+  public editar:boolean= false;
+
+
 
   @Input() rol:string = "";
   @Input() datos_user:any = {};
+
+  constructor(){
+
+  }
 
 }
--- src/app/shared/shared.imports.ts
+++ src/app/shared/shared.imports.ts
@@ -14,7 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import {MatRadioModule} from '@angular/material/radio';
- 
+
 /**
 * SHARED_IMPORTS
 * ---------------------------------------------------------
